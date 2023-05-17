9fa8f410c352931bda2c5c362cc61499586e63b4
Chanjnint21 <limchanjnint@gmail.com>
2023-05-17 17:22:40 +0700

    commented

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+// Disable production tip in console
+Vue.config.productionTip = false;
 
+// Create a new Vue instance
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  // Render function to render the App component
+  render: (h) => h(App),
+}).$mount("#app"); // Mount the instance to a DOM element with an id of 'app'
